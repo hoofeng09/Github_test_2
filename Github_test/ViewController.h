51c2cef1c6212b6849bf1ece5df2279f9806134b
hoofeng09 <hoofeng09@gmail.com>
2015-09-07 15:46:33 -0400

    Initial Commit

---
+++ Github_test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Github_test
+//
+//  Created by Feng Hu on 9/7/15.
+//  Copyright (c) 2015 Feng Hu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Github_test/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Github_test
+//
+//  Created by Feng Hu on 9/7/15.
+//  Copyright (c) 2015 Feng Hu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
